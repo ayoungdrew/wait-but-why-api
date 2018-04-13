14991c662e706402940bd9ff39b5cd70fc2111cf
ayoungdrew <andrewyoung504@gmail.com>
2018-04-13 12:22:23 -0400

    Create seed data
    -Edit seeds.rb and create episodes.csv file

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,3 +9,19 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+require 'csv'
+
+csv_text = File.read(Rails.root.join('lib', 'seeds', 'episodes.csv'))
+csv = CSV.parse(csv_text, :headers => true, :encoding => 'ISO-8859-1')
+csv.each do |row|
+  t = Episode.new
+  t.title = row['title']
+  t.date = row['date']
+  t.why = row['why']
+  t.description = row['description']
+  t.user_id = row['user_id']
+  t.first_or_create
+  puts "#{t.title}, #{t.date} saved"
+end
+
+puts "There are now #{Episode.count} rows in the episodes table"
