d6d7f52857e0b36e58ece4bea3c48ff2de6d7970
ayoungdrew <andrewyoung504@gmail.com>
2018-04-14 10:44:08 -0400

    Added user_id into episode serializer
    -This is to eventually help getting episodes by user_id on client side

--- app/serializers/episode_serializer.rb
+++ app/serializers/episode_serializer.rb
@@ -1,4 +1,4 @@
 class EpisodeSerializer < ActiveModel::Serializer
-  attributes :id, :title, :date, :why, :description
+  attributes :id, :title, :date, :why, :description, :user_id
   has_one :user
 end
