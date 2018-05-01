318e22cdd8b7f4231703445108ef89c2fd957323
ayoungdrew <andrewyoung504@gmail.com>
2018-04-30 20:04:49 -0400

    Friend functionality working!
    Friend functionality working!

--- app/serializers/relationship_serializer.rb
+++ app/serializers/relationship_serializer.rb
@@ -1,6 +1,5 @@
 class RelationshipSerializer < ActiveModel::Serializer
   attributes :id, :follower_id, :followed_id, :follower, :followed
-
   def follower
     object.follower.email
   end
@@ -8,9 +7,5 @@ class RelationshipSerializer < ActiveModel::Serializer
   def followed
     object.followed.email
   end
-
-  # def editable
-  #   scope == object.follower.email
-  # end
 
 end
